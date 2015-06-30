c5150bdf31a75efeb3e8fef3948b890b5eeeeaad
unknown <gio@gio-tinydell.local.ski.org>
2015-06-30 15:41:39 -0700

    more doxygen issues

---
+++ doc/doxygen/search/variables_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['hog_5f',['hog_',['../class_obj_detector.html#a52cbea83a0835b92f6483c6740d3d2b4',1,'ObjDetector']]],
+  ['hogwinsize',['hogWinSize',['../class_detection_params.html#a63590079447139db85009993f8b5ba3d',1,'DetectionParams']]]
+];
