b0af5019a098df049cefe42afebe15f012fbc4c2
unknown <gio@gio-tinydell.local.ski.org>
2015-09-09 14:32:24 -0700

    added new config files and new restroom sign cascade. Fixed input resolution to 640x480 (takes into account frame orientation when resizing

--- src/main.cpp
+++ src/main.cpp
@@ -106,6 +106,10 @@ int main(int argc, char* argv[]){
 				while (vc.read(frame)){
 
 					++frameno;
+					if (frame.size().width > frame.size().height)
+						cv::resize(frame, frame, cv::Size(640, 480));
+					else
+						cv::resize(frame, frame, cv::Size(480, 640));
 
 					auto result = detector.detect(frame, fps);
 					if (dumpPatches)
