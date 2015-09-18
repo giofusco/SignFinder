1cae81f5d0d5f47bb423f251d89261b7c2fee433
Ender Tekin <ender@ski.org>
2015-09-17 22:41:07 -0500

    Changes usage due to Opencv 2.4.11 bug, now requires -i flag to provide an input (otherwise SignFinder -c configfile 1 would inexplicably crash). Fixed some doxygen issues

--- src/MedianFlowTracker.hpp
+++ src/MedianFlowTracker.hpp
@@ -1,5 +1,19 @@
+/*
+ Copyright 2015 The Smith-Kettlewell Eye Research Institute
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 /**
- * @file PatchTracker.hpp
+ * @file MedianFlowTracker.hpp
  *
  *  Created on: @date Mar 12, 2013
  *      Author: @author ender tekin
@@ -11,7 +25,7 @@
 #include <cstdint>
 #include <opencv2/core/core.hpp>
 
-typedef cv::Mat_<std::uint8_t> MatUint8;
+typedef cv::Mat_<std::uint8_t> MatUint8;        ///< grayscale image
 
 /// Median flow tracker.
 /// Based on http://www3.ee.surrey.ac.uk/CVSSP/Publications/papers/Kalal-ICPR-2010.pdf
